6a2d0938a207c26a80ef2739b6a3d22d94bb935f
sz_urbanik <sz.urbanik@mat.net.pl>
2019-03-11 20:04:43 +0100

    Flexible list added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,73 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { id: '1', name: 'Max', age: 28 },
+      { id: '2', name: 'Manu', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 }
+    ],
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const persons = [...this.state.persons];
+    person.name = event.target.value;
+    const person = {...this.state.persons[personIndex]};
+    persons[personIndex] = person;
+    this.setState({ persons: persons })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({person: persons});
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click={() => this.deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age}
+            key={person.id}
+            changed={event => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
     return (
-      <div>
+      <div className='App'>
         <h1>Hi I'm a React app</h1>
-        <Person />
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name</button>
+        { persons }
       </div>
     );
   }
 }
+
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Person.css'
 
-const person = () => {
-    return <p>I'm a P erson!</p>
+const person = props => {
+    return (<div className="Person">
+        <p onClick={ props.click }>I'm a { props.name } and I am { props.age } years old</p>
+        <p>{ props.children }</p>
+        <input onChange={props.changed} value={props.name}></input>
+    </div>)
 }
 
 export default person;
