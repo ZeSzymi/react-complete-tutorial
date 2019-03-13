9803417bf63922229dfe6388497188dd79c2c215
sz_urbanik <sz.urbanik@mat.net.pl>
2019-03-13 20:47:27 +0100

    Css added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
+
+
 class App extends Component {
 
   state = {
@@ -15,9 +17,10 @@ class App extends Component {
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => p.id === id);
-    const persons = [...this.state.persons];
-    person.name = event.target.value;
     const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    
+    const persons = [...this.state.persons];
     persons[personIndex] = person;
     this.setState({ persons: persons })
   }
@@ -30,16 +33,17 @@ class App extends Component {
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({person: persons});
+    this.setState({persons: persons});
   }
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
+      color: 'white',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
     };
 
     let persons = null;
@@ -57,11 +61,23 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className='App'>
         <h1>Hi I'm a React app</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Switch Name</button>
